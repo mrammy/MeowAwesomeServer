c87d472900681c2080fde801db79f4465c843161
Mary <minecraft.mrammy@gmail.com>
2015-10-02 16:52:17 -0400

    Add confirm flag to server launch, won't stop backups so i'll have to manually clean those as needed this is intended incase the rsync as part of updater has some sort of issue.

--- serverstart.sh
+++ serverstart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source ./config.sh
-java -jar -server -Xms8g -Xmx16g -Xmn2G forge-$FORGEVERSION-universal.jar nogui
+java -jar -server -Xms8g -Xmx16g -Xmn2G -Dfml.queryResult=confirm forge-$FORGEVERSION-universal.jar nogui
 unset FORGEVERSION
